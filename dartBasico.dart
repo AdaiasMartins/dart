9b701a6ae8b036a4c197c96dfa7c101f347bc740
AdaiasMartins <110681069+AdaiasMartins@users.noreply.github.com>
2024-10-12 08:38:30 -0300

    estruturas de fluxo

--- dartBasico.dart
+++ dartBasico.dart
@@ -15,7 +15,37 @@ void main(){
   print('${listaDePalavras[0]}, ${listaDePalavras[1]}');
 
   //null-safety
+  //a interrogação é usada para falar que uma variável pode ser nula
   String? nome;
   print(nome);
+
+  //a exclamação pode ser usada para garantir que uma variável não possa ser nula
+  String nomeNaoNulo;
+  //print(nomeNaoNulo!);
+
+  //estruturas de fluxo
+  bool seguirEmFrente = false;
+  if(seguirEmFrente){
+    print("Andar");
+  }else{
+    print("Parar");
+  }
+
+  int valorInt = 5;
+  switch(valorInt){
+    case 0:
+      print("Zero");
+      break;
+    case 1:
+      print("Um");
+      break;
+    case 2:
+      print("Dois");
+      break;
+    default:
+      print("padrão");
+      break;
+      
+  }
 
 }
