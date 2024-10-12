b80cd12960206d2a0b009836a4fb336a5836276b
AdaiasMartins <110681069+AdaiasMartins@users.noreply.github.com>
2024-10-12 08:05:38 -0300

    variáveis

---
+++ variaveis_e_tipos.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main(){
+  String variavelNome = "Adaias";
+  print(variavelNome);
+
+  int numeroInteiro = 10;
+  print(numeroInteiro);
+
+  bool isTrue = true;
+  print(isTrue);
+
+  List<String> listaDePalavras = ["Adaias", "Martins"];
+  print('${listaDePalavras[0]}, ${listaDePalavras[1]}');
+
+  
+}
